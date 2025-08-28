eb180b58cec1b5ca56d80da2882a6974630ecef9
Armando Salazar <asalazar@tpitexas.com>
2025-08-28 15:23:47 -0500

    Batalle un chingo, pero ya pude hacer que funcionara el API, solo funciona en el localserver por el momento, en el liveserver aun no, hare commit y luego intentare subirlo a github....creo que puede ser por eso

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,26 +1,19 @@
-// Import icons (used later for UI rendering)
 import icons from '../img/icons.svg';
+import '../sass/main.scss';
 
-// Get container where recipe will be shown
 const recipeContainer = document.querySelector('.recipe');
 
-const timeout = function (s) {
-  return new Promise(function (_, reject) {
-    setTimeout(function () {
-      reject(new Error(`Request took too long! Timeout after ${s} second`));
-    }, s * 1000);
-  });
-};
+const timeout = s =>
+  new Promise((_, reject) =>
+    setTimeout(() => reject(new Error(`Request took too long! Timeout after ${s} second`)), s * 1000)
+  );
 
-// Async function to fetch recipe data
 const showRecipe = async function () {
   try {
     renderSpinner(recipeContainer);
 
-    const resp = await fetch(
-      'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886'
-    );
-    const data = await resp.json();
+    const res = await fetch('https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886');
+    const data = await res.json();
 
     let recipe = data.data.recipe;
     recipe = {
@@ -40,7 +33,7 @@ const showRecipe = async function () {
   }
 };
 
-const renderSpinner = function (parentEl) {
+const renderSpinner = parentEl => {
   const markup = `
     <div class="spinner">
       <svg>
@@ -52,11 +45,58 @@ const renderSpinner = function (parentEl) {
   parentEl.insertAdjacentHTML('afterbegin', markup);
 };
 
-const renderRecipe = function (recipe) {
+const renderRecipe = recipe => {
   const markup = `
-    <!-- ... your recipe markup here ... same as before ... -->
-  `;
+    <figure class="recipe__fig">
+      <img src="${recipe.image}" alt="${recipe.title}" class="recipe__img" />
+      <h1 class="recipe__title"><span>${recipe.title}</span></h1>
+    </figure>
 
+    <div class="recipe__details">
+      <div class="recipe__info">
+        <svg class="recipe__info-icon"><use href="${icons}#icon-clock"></use></svg>
+        <span class="recipe__info-data recipe__info-data--minutes">${recipe.cookTime}</span>
+        <span class="recipe__info-text">minutes</span>
+      </div>
+      <div class="recipe__info">
+        <svg class="recipe__info-icon"><use href="${icons}#icon-users"></use></svg>
+        <span class="recipe__info-data recipe__info-data--people">${recipe.servings}</span>
+        <span class="recipe__info-text">servings</span>
+      </div>
+    </div>
+
+    <div class="recipe__ingredients">
+      <h2 class="heading--2">Recipe ingredients</h2>
+      <ul class="recipe__ingredient-list">
+        ${recipe.ingredients
+          .map(
+            ing => `
+            <li class="recipe__ingredient">
+              <svg class="recipe__icon"><use href="${icons}#icon-check"></use></svg>
+              <div class="recipe__quantity">${ing.quantity || ''}</div>
+              <div class="recipe__description">
+                <span class="recipe__unit">${ing.unit}</span>
+                ${ing.description}
+              </div>
+            </li>`
+          )
+          .join('')}
+      </ul>
+    </div>
+
+    <div class="recipe__directions">
+      <h2 class="heading--2">How to cook it</h2>
+      <p class="recipe__directions-text">
+        This recipe was carefully designed and tested by
+        <span class="recipe__publisher">${recipe.publisher}</span>. Please check out
+        directions at their website.
+      </p>
+      <a class="btn--small recipe__btn" href="${recipe.sourceUrl}" target="_blank">
+        <span>Directions</span>
+        <svg class="search__icon"><use href="${icons}#icon-arrow-right"></use></svg>
+      </a>
+    </div>
+  `;
   recipeContainer.innerHTML = '';
   recipeContainer.insertAdjacentHTML('afterbegin', markup);
 };
